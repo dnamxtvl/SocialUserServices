744a28f23d8d56719ebcf24b6f8cc7c3799bdf67
namdovan <namdovan@vnpt.vn>
2024-01-26 16:03:04 +0700

    Add param function create token

--- app/Domains/Auth/Jobs/CheckUserBlockLoginTemporaryJob.php
+++ app/Domains/Auth/Jobs/CheckUserBlockLoginTemporaryJob.php
@@ -22,7 +22,10 @@ class CheckUserBlockLoginTemporaryJob
         /** @var User $user */
         if ($user) {
             /** @var BlockUserLoginTemporary $lockUserLoginTemporary */
-            $lockUserLoginTemporary = $user->blockUserLoginTemporary()->where('ip', $this->ip)->first();
+            $lockUserLoginTemporary = $user->blockUserLoginTemporary()
+                ->where('ip', $this->ip)
+                ->first();
+
             if ($lockUserLoginTemporary && ! now()->gt(date: $lockUserLoginTemporary->expired_at)) {
                 throw new LoginWrongPasswordManyException(code: AuthExceptionEnum::LOGIN_WRONG_PASSWORD_MANY->value);
             }
